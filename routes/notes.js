959699e62c7d8941fbdcc3c17f3f3be422fe3566
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-03-12 15:31:51 -0500

    Notes will save on db.json

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,17 @@
 const notes = require("express").Router();
+const fs = require("fs");
 
 // GET Route for retrieving notes info
 notes.get("/", (req, res) => {
   // TODO: pass through db.json and send it!
   console.info("party");
-  res.json("./db/db.json");
+  fs.readFile("./db/db.json", "utf8", (err, notes) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(JSON.parse(notes));
+    }
+  });
 });
 
 notes.post("/", (req, res) => {
@@ -15,7 +22,31 @@ notes.post("/", (req, res) => {
   const newNote = {
     title,
     text,
+    id: Date.now(),
   };
+
+  // Obtain existing reviews
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      // Add a new review
+      parsedNotes.push(newNote);
+
+      // Write updated reviews back to the file
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info("Successfully updated Notes!")
+      );
+    }
+  });
 });
 
 module.exports = notes;
